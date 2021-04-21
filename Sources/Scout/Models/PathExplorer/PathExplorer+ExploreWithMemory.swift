bbcf7bde7d464561de96ec4995a7479e035cad8d
Alexis Bridoux <alexis1bridoux@gmail.com>
2021-04-21 09:50:33 +0200

    Hid Path element parsers behind namespace

--- Sources/Scout/Models/PathExplorer/PathExplorer+ExploreWithMemory.swift
+++ Sources/Scout/Models/PathExplorer/PathExplorer+ExploreWithMemory.swift
@@ -14,21 +14,21 @@ extension PathExplorer {
     /// - Parameters:
     ///   - paths: The paths to get
     ///   - block: The block to execute with the explorer retrieved for the path
-    func explore(paths: [Path], using block: (Result<Self, ExplorerError>) throws -> Void) rethrows {
-        let history = History(origin: self)
-        try paths.forEach { try  history.explore(path: $0, using: block) }
+    func exploreWithMemory(paths: [Path], using block: (Result<Self, ExplorerError>) throws -> Void) rethrows {
+        let history = GetMemory(origin: self)
+        try paths.forEach { try history.explore(path: $0, using: block) }
     }
 
-    func exploreReduce<T>(initial: T, paths: [Path], using transform: (_ result: T, _ explorer: Result<Self, ExplorerError>) throws -> T) rethrows -> T {
+    func reduceWithMemory<T>(initial: T, paths: [Path], using transform: (_ result: T, _ explorer: Result<Self, ExplorerError>) throws -> T) rethrows -> T {
         var result = initial
-        try explore(paths: paths) { (explorer) in
+        try exploreWithMemory(paths: paths) { (explorer) in
             result = try transform(result, explorer)
         }
         return result
     }
 }
 
-private final class History<Explorer: PathExplorer> {
+private final class GetMemory<Explorer: PathExplorer> {
     let origin: Explorer
     private(set) var lastExploredPath: Slice<Path> = Slice(Path.empty)
     private(set) var lastExplorers: ArraySlice<Explorer> = []
@@ -39,7 +39,7 @@ private final class History<Explorer: PathExplorer> {
     }
 
     func explore(path: Path, using exploringHandler: (Result<Explorer, ExplorerError>) throws -> Void) rethrows {
-        // explorer only the difference
+        // explore only the difference
         let differenceToExplore = suffixDifference(with: path)
         var currentExplorer = lastExplorer
 
